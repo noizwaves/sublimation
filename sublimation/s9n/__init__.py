a2ae7e24135b9569a5021d79239b20d5ee288f4c
Adam Neumann <adam@noizwaves.com>
2013-05-30 15:48:53 +1000

    Expanded resource types

--- sublimation/s9n/__init__.py
+++ sublimation/s9n/__init__.py
@@ -5,7 +5,7 @@ A shortened namespace for referencing Sublimation Cf wrappers.
 __all__ = ['Template',
            'Parameter', 'KeyPairParameter', 'SecurityGroupParameter', 'InstanceTypeParamater',
            'Mapping', 'InstanceTypeToArch64BitOnlyMapping', 'RegionToAmiUbuntu12042Mapping',
-           'Resource',
+           'Resource', 'Topic', 'Instance', 'AutoscaleCpuHighAlarm',
            'Output',
            'find_in_map', 'get_azs', 'get_attr', 'base64', 'join', 'ref', 'as_joined_base64',
            'is_bash_useful', 'load_template', 'load_script']
@@ -13,7 +13,7 @@ __all__ = ['Template',
 from ..templates import Template
 from ..parameters import Parameter, KeyPairParameter, SecurityGroupParameter, InstanceTypeParamater
 from ..mappings import Mapping, InstanceTypeToArch64BitOnlyMapping, RegionToAmiUbuntu12042Mapping
-from ..resources import Resource
+from ..resources import Resource, Topic, Instance, AutoscaleCpuHighAlarm
 from ..outputs import Output
 
 from ..helpers import find_in_map, get_azs, get_attr, base64, join, ref, as_joined_base64
