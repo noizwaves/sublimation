4becef5c6de8953e5493ac1dabcb2812e5bde350
Adam Neumann <adam@noizwaves.com>
2013-05-30 15:28:50 +1000

    Expanded mapping types

--- sublimation/s9n/__init__.py
+++ sublimation/s9n/__init__.py
@@ -4,7 +4,7 @@ A shortened namespace for referencing Sublimation Cf wrappers.
 
 __all__ = ['Template',
            'Parameter', 'KeyPairParameter', 'SecurityGroupParameter', 'InstanceTypeParamater',
-           'Mapping',
+           'Mapping', 'InstanceTypeToArch64BitOnlyMapping', 'RegionToAmiUbuntu12042Mapping',
            'Resource',
            'Output',
            'find_in_map', 'get_azs', 'get_attr', 'base64', 'join', 'ref', 'as_joined_base64',
@@ -12,7 +12,7 @@ __all__ = ['Template',
 
 from ..templates import Template
 from ..parameters import Parameter, KeyPairParameter, SecurityGroupParameter, InstanceTypeParamater
-from ..mappings import Mapping
+from ..mappings import Mapping, InstanceTypeToArch64BitOnlyMapping, RegionToAmiUbuntu12042Mapping
 from ..resources import Resource
 from ..outputs import Output
 
